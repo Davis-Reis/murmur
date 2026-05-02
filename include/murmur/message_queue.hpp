5c3d45168e8b31044d53f408c07e0cc9f1445cf7
Davis Reis <davisreis1@gmail.com>
2026-05-02 15:23:44 -0700

    added docs to functions and class, finished wait_and_pop()

--- include/murmur/message_queue.hpp
+++ include/murmur/message_queue.hpp
@@ -4,26 +4,56 @@
 
 namespace murmur {
 
+/*
+ * Wrapper for queue.
+ * Since I'm going to have some semi-complex race conditions for multiple threads accessing a single queue,
+ * I elected to make a wrapper class for queue which holds private mutex and cv variables that aren't exposed
+ * to callers.
+ */
 template <typename T>
 class MessageQueue {
 public:
     MessageQueue() = default;
     
-    // Disallow copying of MessageQueue (mutexes and condition_variables don't have copy constructors so it would result in a compile error)
+    // Disallow copying of MessageQueue
+    // (mutexes and condition_variables don't have copy constructors so it would result in a compile error)
     MessageQueue(const MessageQueue&) = delete;
     MessageQueue& operator = (const MessageQueue&) = delete;
+
+    /**
+     * Pushes a value of type T onto this queue.
+     * Equivalent to queue.push(), with added mutex guarding and cv notification
+     */
     void push(T value) {
         // RAII on lock to make sure it destructs as soon as I don't need it
         {
             std::lock_guard<std::mutex> lock(mutex_);
             queue_.push(T);
-        } // RAII unlocks mutex
+        } // RAII unlocks mutex then destroys it
         cv_.notify_one();
     }
 
-    // wait_and_pop could remain waiting until queue is destroyed, so return is optional
+    /**
+     * waits for an item to be pushed onto queue, when notified store item and pop it
+     * Returns   T value   ||  nullopt
+     *        (item found) || (shutdown)
+     */
     std::optional<T> wait_and_pop() {
-        return NULL;
+        std::unique_lock<std::mutex> lock(mutex_);
+        // while queue is empty AND we're not shut down, wait on cv
+        cv_.wait(lock, [this] { return !queue_.empty() || shutdown_; });
+
+        if (queue_.is_empty()) {
+            // condition_variable was woken up by shutdown and not a new item
+            return std::nullopt;
+        }
+        
+        // condition_variable was woken up by list being populated not shutdown
+        // move ownership
+        T value = std::move(queue_.front());
+        // get rid of null pointer
+        queue_.pop();
+        return value;
     }
 
     // try_pop could opperate on an empty queue so return is optional
@@ -40,6 +70,7 @@ private:
     mutable std::mutex mutex_;
     std::queue queue_;
     std::condition_variable cv_;
+    std::shutdown_ = false;
 
 };
 
