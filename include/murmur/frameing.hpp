ba57005398b238b28fe4208d5177422d9462e8a5
Davis Reis <davisreis1@gmail.com>
2026-06-13 19:01:53 -0600

    made doc for read and write framing

--- include/murmur/frameing.hpp
+++ include/murmur/frameing.hpp
@@ -2,6 +2,20 @@
 
 #include "socpp.hpp"
 
+namespace murmur {
+
+// Writes a databuffer to a socket
+// Params:
+//          sock: socket to be writing to
+//          data: void pointer to data
+//          size: size in bytes of the payload
+// Returns void
 void write_framed(Socket& sock, const void* data, std::size_t size);
 
+// Reads all bytes available in socket
+// Params:
+//          sock: socket to read from
+// Returns vector containing bytes read
 std::vector<std::uint8_t> read_framed(Socket& sock);
+
+} // name space
