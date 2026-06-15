3851794806080383ddbc538fd4a325290b09e260
Davis Reis <davisreis1@gmail.com>
2026-06-15 16:14:18 -0600

    added max size exception, corrected write all call

--- src/framing.cpp
+++ src/framing.cpp
@@ -1,15 +1,16 @@
 #include "framing.hpp"
+#include <stdexcept>
 
 namespace murmur {
 
     void write_framed(Socket& sock, const void* data, std::size_t size) {
+        if (size > MAX_MESSAGE) {
+            throw std::length_error("write_framed: message exceeds MAX_MESSAGE");
+        }
         std::uint32_t len = static_cast<const std::uint32_t>size;
         std::uint32_t header = htonl(len);
-        try {
-            Socket::write_all(sock, &header, 4);
-            Socket::write_all(sock, data, len);
-        } catch (err) {
-            std::cerr << "write_framed failed at Socket::write_all: " << err << std::endl;
-        }
-    }
+        sock.write_all(&header, sizeof(header));
+        sock.write_all(data, len);
 }
+
+} // namespace
