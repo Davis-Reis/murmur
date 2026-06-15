e6f1fa7b37ccb927a36695e50c82bab2bef5add9
Davis Reis <davisreis1@gmail.com>
2026-06-15 15:15:02 -0600

    added type casting to size

--- src/framing.cpp
+++ src/framing.cpp
@@ -3,10 +3,10 @@
 namespace murmur {
 
     void write_framed(Socket& sock, const void* data, std::size_t size) {
-        size_t len = size - 4;
-        int header = htonl(len);
+        std::uint32_t len = static_cast<const std::uint32_t>size;
+        std::uint32_t header = htonl(len);
         try {
-            Socket::write_all(sock, &header, size);
+            Socket::write_all(sock, &header, 4);
             Socket::write_all(sock, data, len);
         } catch (err) {
             std::cerr << "write_framed failed at Socket::write_all: " << err << std::endl;
