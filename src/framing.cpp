d328d0e80ad4c4edbb8819552e2ecd6a9ec50491
Davis Reis <davisreis1@gmail.com>
2026-06-15 14:58:04 -0600

    made try catch to catch connection and socket errs

--- src/framing.cpp
+++ src/framing.cpp
@@ -5,6 +5,11 @@ namespace murmur {
     void write_framed(Socket& sock, const void* data, std::size_t size) {
         size_t len = size - 4;
         int header = htonl(len);
-        Socket::write_all(sock, &header, size);
-        Socket::write_all(sock, data, len);
+        try {
+            Socket::write_all(sock, &header, size);
+            Socket::write_all(sock, data, len);
+        } catch (err) {
+            std::cerr << "write_framed failed at Socket::write_all: " << err << std::endl;
+        }
+    }
 }
