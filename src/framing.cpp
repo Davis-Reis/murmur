8e5cdf619ebeb57a59cc68d5e24285477929fc92
Davis Reis <davisreis1@gmail.com>
2026-06-19 11:40:08 -0600

    fixed compile error

--- src/framing.cpp
+++ src/framing.cpp
@@ -8,7 +8,7 @@ namespace murmur {
         if (size > MAX_MESSAGE) {
             throw std::length_error("write_framed: message exceeds MAX_MESSAGE");
         }
-        std::uint32_t len = static_cast<const std::uint32_t>size;
+        std::uint32_t len = static_cast<std::uint32_t>(size);
         std::uint32_t header = htonl(len);
         sock.write_all(&header, sizeof(header));
         sock.write_all(data, len);
