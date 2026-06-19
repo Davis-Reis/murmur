7302d0d539c14804f272f3c17d600604f2124359
Davis Reis <davisreis1@gmail.com>
2026-06-19 11:38:10 -0600

    made read_framed

--- src/framing.cpp
+++ src/framing.cpp
@@ -1,5 +1,6 @@
 #include "framing.hpp"
 #include <stdexcept>
+#include <vector>
 
 namespace murmur {
 
@@ -11,6 +12,24 @@ namespace murmur {
         std::uint32_t header = htonl(len);
         sock.write_all(&header, sizeof(header));
         sock.write_all(data, len);
-}
+    }
+
+    std::vector<std::uint8_t> read_framed(Socket& sock) {
+        // Read the header to see how big the next message is
+        std::uint32_t header;
+        sock.read_exact(&header, sizeof(header));
+        std::uint32_t len = ntohl(header);
+
+        if (len > MAX_MESSAGE) {
+            throw std::length_error("read_framed: incoming message exceeds MAX_MESSAGE");
+        }
+
+        // Store message after header with size specified in header
+        std::vector<std::uint8_t> buffer(len);
+        sock.read_exact(buffer.data(), len);
+
+        return buffer;
+    }
+
 
 } // namespace
